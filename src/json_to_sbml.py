584295e21fe21c5cbeeaf37fd9ec47116d2566ea
AngadSC <104170661+AngadSC@users.noreply.github.com>
2026-03-04 21:56:12 -0700

    this will solve that stupid fast error

--- src/json_to_sbml.py
+++ src/json_to_sbml.py
@@ -813,6 +813,8 @@ def build_sbml(
         if plan.get("compartment_id"):
             rxn.setCompartment(plan["compartment_id"])
         rxn.setReversible(False)
+        # SBML Level 3 Version 1 requires an explicit 'fast' attribute on reactions.
+        rxn.setFast(False)
         reactant_counts = Counter(plan["reactants"])
         for sid, stoich in sorted(reactant_counts.items()):
             ref = rxn.createReactant()
